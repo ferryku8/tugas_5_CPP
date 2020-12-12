75b2207ee94690de603218d752d42d1518b28693
Ferry Kurniawan <ferryku8@gmail.com>
2020-12-12 19:19:11 +0700

    TUGAS 5 C++
    Mengumpulkan Tugas 5 C++

---
+++ tugas 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	float x,y,z;
+	cout<<"Inputkan Nilai Yang Ingin Dipangkatkan : ";
+	cin>>x;
+	cout<<"Inputkan Nilai Pangkat : ";
+	cin>>y;
+	z = pow(x,y);
+	cout<<"Hasil Per Pangkatan : "<<z;
+}
